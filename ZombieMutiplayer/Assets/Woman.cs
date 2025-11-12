5fbdd824319624669bddf9a04c1170d832109c67
domaumrD <98871748+domaumrD@users.noreply.github.com>
2025-11-12 16:47:40 +0900

    이동
    이동

--- ZombieMutiplayer/Assets/Woman.cs
+++ ZombieMutiplayer/Assets/Woman.cs
@@ -5,12 +5,36 @@ using Unity.VisualScripting;
 
 public class Woman : MonoBehaviourPun
 {
+    float h;
+    float v;
 
+    float moveSpeed = 5f;
+    public Animator animator;
 
     private void Update()
     {
         if (photonView.IsMine == false)
             return;
+
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(h, 0, v);
+   
+
+        if (dir != Vector3.zero)
+        {           
+            Quaternion rot = Quaternion.LookRotation(dir);
+            transform.rotation = rot;
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            animator.SetInteger("Status", 1);
+        }
+        else
+        {
+            animator.SetInteger("Status", 0);
+        }
+       
+
     }
 
 }
