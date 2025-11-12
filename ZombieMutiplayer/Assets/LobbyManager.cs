18331e21b07d6094cbfd219b560451aba77b2337
domaumrD <98871748+domaumrD@users.noreply.github.com>
2025-11-12 13:52:35 +0900

    룸 접속
    룸 접속

---
+++ ZombieMutiplayer/Assets/LobbyManager.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+using Photon.Realtime;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    private string gameVersion = "1";
+    public Button btn;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+        PhotonNetwork.GameVersion = gameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+
+        btn.interactable = false;
+        btn.onClick.AddListener(() =>
+        {
+            Debug.Log("∑Î ¡¢º” ø‰√ª");
+            Connect();
+        });
+    }
+
+    private void Connect()
+    {
+        Debug.Log($"Isconnected:  {PhotonNetwork.IsConnected}");
+
+
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+
+        }
+
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        btn.interactable = true;
+        Debug.Log("OnConnectedToMaster");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("OnDisconnected");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("OnJoinedRoom");
+        Debug.Log(PhotonNetwork.IsMasterClient);
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.Log($"OnJoinRoomFailed {returnCode}, {message}");
+
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 2 });
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("OnCreatedRoom");
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log($"OnCreateRoomFailed {returnCode}, {message}");
+    }
+
+}
