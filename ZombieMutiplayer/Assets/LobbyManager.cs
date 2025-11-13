a8d620c899275cb25b55451e53288c02c0f5a46d
domaumrD <98871748+domaumrD@users.noreply.github.com>
2025-11-13 15:04:52 +0900

    로비 수정
    로비 수정

--- ZombieMutiplayer/Assets/LobbyManager.cs
+++ ZombieMutiplayer/Assets/LobbyManager.cs
@@ -1,50 +1,82 @@
 ﻿using NUnit.Framework.Interfaces;
 using Photon.Pun;
 using Photon.Realtime;
+using System;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class LobbyManager : MonoBehaviourPunCallbacks
 {
     private string gameVersion = "1";
-    public Button btn;
+    public GameObject inputNickName;
+
     public string myName;
     private Player[] players = PhotonNetwork.PlayerList;
+    public TMP_Text lobbyText;
+
+    public UINicknameView uiNicknameView;
+    public GameObject LobbyRoomList;
+    public TMP_Text NolobbyRoomText;
+
+    public Button createRoomBtn;
 
     void Start()
     {
+        lobbyText.text = "Title";
         PhotonNetwork.GameVersion = gameVersion;
-        PhotonNetwork.ConnectUsingSettings();
+        inputNickName.SetActive(true);
+        LobbyRoomList.SetActive(false);
+        NolobbyRoomText.gameObject.SetActive(false);
+        createRoomBtn.gameObject.SetActive(false);
+        uiNicknameView.onClickSubmit = SetNickName;
 
-        btn.interactable = false;
-        btn.onClick.AddListener(() =>
-        {
-            Debug.Log("룸 접속 요청");
-            Connect();
-        });
     }
 
     private void Connect()
     {
+
         Debug.Log($"Isconnected:  {PhotonNetwork.IsConnected}");
 
         if (PhotonNetwork.IsConnected)
         {
+            inputNickName.SetActive(false);
 
             Debug.Log("test");
-            PhotonNetwork.JoinRandomRoom();
+            //PhotonNetwork.JoinRandomRoom();
+          
+            JoinLobby();
         }
         else
         {
             Debug.Log("failed");
             PhotonNetwork.ConnectUsingSettings();
         }
-    }   
+    }
+
+    public void JoinLobby()
+    {
+        PhotonNetwork.JoinLobby();
+        OnJoinedLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        lobbyText.text = "Lobby";
+        LobbyRoomList.SetActive(true);
+        NolobbyRoomText.gameObject.SetActive(true);
+        createRoomBtn.gameObject.SetActive(true);
+        Debug.Log("I'm in Lobby");  
+    }
+
 
     public override void OnConnectedToMaster()
     {
-        btn.interactable = true;
+
+        //btn.gameObject.SetActive(true);
+        //btn.interactable = true;
+      
         Debug.Log("OnConnectedToMaster");
     }
 
@@ -59,14 +91,14 @@ public class LobbyManager : MonoBehaviourPunCallbacks
         Debug.Log("OnJoinedRoom");
         Debug.Log($"IsMasterClient: {PhotonNetwork.IsMasterClient}");
 
-        PhotonNetwork.NickName = myName;
+        //PhotonNetwork.NickName = myName;
 
         for (int i = 0; i < players.Length; i++)
         {
             Debug.Log($"{players[i].NickName} 입장");
         }
 
-        PhotonNetwork.LoadLevel("Main");
+        //PhotonNetwork.LoadLevel("Main");
     }
 
     public override void OnJoinRandomFailed(short returnCode, string message)
@@ -84,6 +116,14 @@ public class LobbyManager : MonoBehaviourPunCallbacks
     public override void OnCreateRoomFailed(short returnCode, string message)
     {
         Debug.Log($"OnCreateRoomFailed {returnCode}, {message}");
+    }
+
+    public void SetNickName(string nickName)
+    {
+        myName = nickName;
+        PhotonNetwork.NickName = nickName;
+        PhotonNetwork.ConnectUsingSettings();
+        Connect();
     }
 
 }
